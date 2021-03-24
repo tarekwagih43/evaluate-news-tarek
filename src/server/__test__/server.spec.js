6ccad27fcaa4364e006694f424b9ac3754d86474
tarekwagih43 <tarekwagih43@gmail.com>
2021-03-24 23:50:24 +0200

    Update server.spec.js

--- src/server/__test__/server.spec.js
+++ src/server/__test__/server.spec.js
@@ -3,12 +3,15 @@ const app = require('../index')
 const request = require('supertest')
 
 describe('Post Endpoint /add-url', () => {
-    it('Post Endpoint /add-url', async () => {
-        const res = await request(app).post('/add-url')
-        .send({
-            url: 'https://jestjs.io/docs/asynchronous',
+    it('renders without crashing', () => {
+        beforeEach(async (done) => {
+            const res = await request(app)
+                .post('/add-url')
+                .send({ url: 'https://jestjs.io/docs/asynchronous' })
         })
-        expect(res.statusCode).toEqual(201)
-        expect(res.body).toHaveProperty('post')
+        afterAll(async done => {
+            await expect(res.statusCode).toEqual(201)
+            await expect(res.body).toHaveProperty('post')
+        })
     })
 })
