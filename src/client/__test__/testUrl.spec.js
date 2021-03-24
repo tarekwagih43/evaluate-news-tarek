ca8a4cba45458fd6bdef5096acce6e1e829ed38f
tarekwagih43 <tarekwagih43@gmail.com>
2021-03-24 20:22:17 +0200

    reviewer comments (2)

--- src/client/__test__/testUrl.spec.js
+++ src/client/__test__/testUrl.spec.js
@@ -1,16 +1,19 @@
 import CheckURL from '../js/checkURL'
 
-describe('Test check url functionality', () => {
-    test('Testing the checkUrl function defined or not', () => {
+describe('Function if Defined or not', () => {
+    test('checkUrl function defined or not', () => {
         expect(CheckURL()).toBeDefined()
     })
 
-    test('Testing the checkUrl function return false for invalid url', () => {
+})
+
+describe('Test Response Value', () => {
+    test('function return false for invalid url', () => {
         let url = 'test false'
         expect(CheckURL(url)).toBeFalsy()
     })
 
-    test('Testing the checkUrl function return true for valid url', () => {
+    test('checkUrl function return true for valid url', () => {
         let url = 'http://text.test'
         expect(CheckURL(url)).toBeTruthy()
     })
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,38 +1,32 @@
 import CheckURL from './checkURL'
 
-const post = async (url = '', data = {}) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    try {
-        return await response.json()
-    } catch (error) {
-        console.log(error)
-    }
-}
-
 const handleSubmit = async () => {
 
-    let url = document.querySelector('#article-url').value
+    let url = await document.querySelector('#article-url').value
     if(CheckURL(url)) {
-        console.log(url)
-        post("http://localhost:8081/add-url", {url} )
-        .then(data => {
-        console.log('test:', data)
-        document.querySelector('#text').innerHTML = `Text: ${data.text}`
-        document.querySelector('#agreement').innerHTML = `agreement: ${data.agreement}`
-        document.querySelector('#subjectivity').innerHTML = `subjectivity: ${data.subjectivity}`
-        document.querySelector('#confidence').innerHTML = `confidence: ${data.confidence}`
-        document.querySelector('#irony').innerHTML = `irony: ${data.irony}`
-        document.querySelector('#score_tag').innerHTML = `score_tag: ${data.score_tag}`
-
+        const response = await fetch('http://localhost:8081/add-url', {
+            method: 'POST',
+            credentials: 'same-origin',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({'url': url})
         })
+        try {
+            const data = await response.json()
+            const send = (data) => {
+                document.querySelector('#text').innerHTML = `Text: ${data.text}`
+                document.querySelector('#agreement').innerHTML = `agreement: ${data.agreement}`
+                document.querySelector('#subjectivity').innerHTML = `subjectivity: ${data.subjectivity}`
+                document.querySelector('#confidence').innerHTML = `confidence: ${data.confidence}`
+                document.querySelector('#irony').innerHTML = `irony: ${data.irony}`
+                document.querySelector('#score_tag').innerHTML = `score_tag: ${data.score_tag}`
+                }
+            return await send(data)
+        } catch (error) {
+            console.log(error)
+        }
     } else {
         alert('Please Enter Valid URL!')
     }
